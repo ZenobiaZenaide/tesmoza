d178a472cbe21c1c8868535ae0406cb7f737f877
AthiyaPuteri <athiyaputeri19@gmail.com>
2024-01-30 10:48:36 +0700

    add web

--- moza/routes/web.php
+++ moza/routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\DivisionLeaderController;
 use App\Http\Controllers\UnitLeaderController;
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +17,35 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
-
-Route::get('/', function () {
-    return view('login');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/',[LoginController::class,'index'])->name('login');
+    Route::post('/',[LoginController::class,'login']);
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return redirect('/divisionleader');
+});
+
+Route::middleware(['auth'])->group(function() {
+
+    // Division Leader Access
+    // Route::get('/divisionleader',[DivisionLeaderController::class,'divisionleader'])->middleware('userAccess:divisionleader');
+    Route::group(['middleware' => ['userAccess:divisionleader']], function () {
+        Route::get('/divisionleader', [DivisionLeaderController::class, 'divisionleader']);
+    });
+
+    // Unit Leader Access
+    // Route::get('/unitleader',[UnitLeaderController::class,'unitleader'])->middleware('userAccess:unitleader');
+    Route::group(['middleware' => ['userAccess:unitleader']], function () {
+        Route::get('/unitleader', [UnitLeaderController::class, 'unitleader']);
+    });
+
+    // Employee Access
+    // Route::get('/employee',[EmployeeController::class,'employee'])->middleware('userAccess:employee');
+    Route::group(['middleware' => ['userAccess:employee']], function () {
+        Route::get('/employee', [EmployeeController::class, 'employee']);
+    });
+
+    //Log Out
+    Route::get('/logout',[LoginController::class,'logout']);
 });
